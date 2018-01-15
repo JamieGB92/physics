df9b406c810650b4ed985662df7cb2b722bf513f
JamieGB92 <jamie.g.copeland@gmail.com>
2018-01-15 13:53:05 -0500

    qos
    working on stuff

--- physics final/Assets/fallplate.cs
+++ physics final/Assets/fallplate.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class fallplate : MonoBehaviour {
 
+    public float timer;
+   private IEnumerator countDown(float t)
+    {
+
+        yield return new WaitForSeconds(20);
+        GetComponentInParent<Rigidbody>().useGravity = false;
+    }
 	// Use this for initialization
-	void Start () {
+	void Start() { }
 		
-	}
+	
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+       
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag=="Player")
+        {
+
+            countDown(timer);
+        }
+    }
 }
